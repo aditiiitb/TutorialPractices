8483b93b1b9c795397fe183ea53cf10854e7d246
Aditi Goyal <aditiiitb@gmail.com>
2018-11-24 13:14:33 -0800

    Several components added

---
+++ FrontendAngular/myapp/src/app/app-routing/routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
+import { HomeComponent } from '../home/home.component';
+import { ContactComponent } from '../contact/contact.component';
+import { AboutComponent } from '../about/about.component';
+import { MenuComponent } from '../menu/menu.component';
+
+export const routes: Routes = [
+{
+  path: 'home', component: HomeComponent
+},
+{
+  path: 'menu', component: MenuComponent
+},
+{
+  path: 'about', component: AboutComponent
+},
+{
+  path: 'contact', component: ContactComponent
+},
+{path: '', redirectTo: '/home', pathMatch: 'full'}
+];
--- FrontendAngular/myapp/src/app/app.module.ts
+++ FrontendAngular/myapp/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,14 +13,22 @@ import {MatButtonModule} from '@angular/material/button';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 import { DishService} from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
