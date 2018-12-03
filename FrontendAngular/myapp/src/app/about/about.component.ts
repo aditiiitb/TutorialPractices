d9404aa57d001bbb6f6802d3f1ce86b5ef9c304e
Aditi Goyal <aditiiitb@gmail.com>
2018-12-02 20:07:15 -0800

    Edited About

--- FrontendAngular/myapp/src/app/about/about.component.ts
+++ FrontendAngular/myapp/src/app/about/about.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Leader } from '../shared/leader';
-import { LeaderService } from '../services/leader.service';
-import {flyInOut, expand} from '../animations/app.animation';
+import { Component, OnInit, Inject } from "@angular/core";
+import { Leader } from "../shared/leader";
+import { LeaderService } from "../services/leader.service";
+import { flyInOut, expand } from "../animations/app.animation";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.scss"],
   host: {
-    '[@flyInOut]': 'true',
-    'style' : 'display: block;'
+    "[@flyInOut]": "true",
+    style: "display: block;"
   },
-  animations: [
-    flyInOut(),
-    expand()
-  ]
+  animations: [flyInOut(), expand()]
 })
 export class AboutComponent implements OnInit {
-
   leaders: Leader[];
 
-  constructor(private leaderservice: LeaderService) {  }
+  constructor(
+    private leaderservice: LeaderService,
+    @Inject("BaseURL") private BaseURL
+  ) {}
 
   ngOnInit() {
-   this.leaderservice.getLeaders().subscribe((leaders) => this.leaders=leaders);
+    this.leaderservice
+      .getLeaders()
+      .subscribe(leaders => (this.leaders = leaders));
   }
-
 }
