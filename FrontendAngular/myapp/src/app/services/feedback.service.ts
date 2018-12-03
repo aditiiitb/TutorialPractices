a707675e259e6f218a48ead3d841d1567eabffd3
Aditi Goyal <aditiiitb@gmail.com>
2018-12-02 20:19:13 -0800

    Store feedback into server

---
+++ FrontendAngular/myapp/src/app/services/feedback.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Feedback } from '../shared/feedback';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { baseURL } from "../shared/baseurl";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedbackService {
+
+  constructor(private http: HttpClient) { }
+
+  public submitFeedback(feedback: Feedback | any): Observable<Feedback> {
+    console.log(feedback);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type" : "application/json"
+      })
+    };
+    return this.http.post<Feedback>(
+      baseURL+ "feedback",
+      feedback,
+      httpOptions
+    )
+  }
+}
